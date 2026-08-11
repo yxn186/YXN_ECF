df97d5a5dfa36043be1c2d79d73812a95f441f4c
yxn186 <1412535486@qq.com>
2026-08-11 23:03:51 +0800

    完成

--- bsp/USB/bsp_usb.h
+++ bsp/USB/bsp_usb.h
@@ -1,13 +1,6 @@
 /**
- * @file bsp_usb.cpp
- * @brief 参照yssickjgd
- *
- */
-
-/**
- * 双缓冲接收部分位于usbd_cdc_if.c中, 需要在usbd_cdc_if.c中, 如有需要可在此处修改
- * 具体函数为 static int8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len)
- * 此外, 为保证编译通过, user-include部分中, 需要额外声明 typedef int bool
+ * @file bsp_usb.h
+ * @brief USB CDC双缓冲收发接口
  */
 
 #ifndef DRV_USB_H
@@ -19,20 +12,18 @@ extern "C" {
 
 /* Includes ------------------------------------------------------------------*/
 
-//#include "1_Middleware/System/Timestamp/sys_timestamp.h"
 #include "usbd_cdc_if.h"
 #include <string.h>
 #include "stdbool.h"
 /* Exported macros -----------------------------------------------------------*/
 
-// 缓冲区字节长度
+//USB CDC单次接收缓冲区长度
 #define USB_BUFFER_SIZE 512
 
 /* Exported types ------------------------------------------------------------*/
 
 /**
- * @brief UART通信接收回调函数数据类型
- *
+ * @brief USB接收回调函数类型
  */
 typedef void (*USB_Callback)(uint8_t *Buffer, uint16_t Length);
 
@@ -41,16 +32,15 @@ typedef void (*USB_Callback)(uint8_t *Buffer, uint16_t Length);
  */
 struct Struct_USB_Manage_Object
 {
-    UART_HandleTypeDef *UART_Handler;
     USB_Callback Callback_Function;
 
-    // 双缓冲适配的缓冲区以及当前激活的缓冲区
+    //双缓冲接收区
     uint8_t Rx_Buffer_0[USB_BUFFER_SIZE];
     uint8_t Rx_Buffer_1[USB_BUFFER_SIZE];
     uint8_t *Rx_Buffer_Active;
     uint8_t *Rx_Buffer_Ready;
 
-    // 接收时间戳
+    //最近一次接收时间戳
     uint64_t Rx_Time_Stamp;
 };
 
@@ -62,16 +52,42 @@ extern struct Struct_USB_Manage_Object USB0_Manage_Object;
 
 /* Exported function declarations --------------------------------------------*/
 /**
- * @brief 初始化USB
+ * @brief 默认USB接收回调
  *
- * @param Callback_Function 处理回调函数
+ * @param Buffer USB接收缓冲区
+ * @param Length 本次接收数据长度
  */
-void USB_Init(USB_Callback USB_Callback_Funtion);
+void USB_Serial_Callback(uint8_t *Buffer,uint16_t Length);
 
-uint8_t USB_Transmit_Data(uint8_t *Data, uint16_t Length);
+/**
+ * @brief 初始化USB接收缓冲区和数据回调
+ *
+ * @param Callback_Function USB数据处理回调函数
+ */
+void USB_Init(USB_Callback Callback_Function);
 
-void USB_Printf(const char *format, ...);
+/**
+ * @brief 通过USB CDC发送数据
+ *
+ * @param Data 发送数据缓冲区
+ * @param Length 发送数据长度
+ * @return uint8_t USB设备栈返回状态
+ */
+uint8_t USB_Transmit_Data(uint8_t *Data,uint16_t Length);
 
+/**
+ * @brief 通过USB CDC发送格式化文本
+ *
+ * @param Format printf格式字符串
+ * @param ... 格式化参数
+ */
+void USB_Printf(const char *Format,...);
+
+/**
+ * @brief 完成本次USB接收并重新挂载下一块缓冲区
+ *
+ * @param Size 本次接收数据长度
+ */
 void USB_ReceiveCallback(uint16_t Size);
 
 #ifdef __cplusplus
